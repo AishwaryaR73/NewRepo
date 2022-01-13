6450d89d93efbc71523eb60bd2c24faefd54aceb
AishwaryaR73 <97290902+AishwaryaR73@users.noreply.github.com>
2022-01-13 17:34:41 +0530

    new protractor
    new protractor

---
+++ LT_Protactor/conf1.js
@@ -0,0 +1,49 @@
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
+var reporter = new HtmlScreenshotReporter({
+  dest: 'LT_target/screenshots',
+  filename: 'my-report.html'
+});
+
+
+
+// An example configuration file.
+exports.config = {
+  directConnect: true,
+
+  specs: ['TestScript.js'],
+
+// Setup the report before any tests start
+beforeLaunch: function() {
+  return new Promise(function(resolve){
+    reporter.beforeLaunch(resolve);
+  });
+},
+
+// Assign the test reporter to each running instance
+onPrepare: function() {
+  jasmine.getEnv().addReporter(reporter);
+
+
+  var AllureReporter = require('jasmine-allure-reporter');
+  jasmine.getEnv().addReporter(new AllureReporter({
+    resultsDir: 'allure-results'
+  }));
+
+
+
+},
+
+// Close the report after all tests finish
+afterLaunch: function(exitCode) {
+  return new Promise(function(resolve){
+    reporter.afterLaunch(resolve.bind(this, exitCode));
+  });
+}
+
+
+
+
+
+
+};
